b670799d98e7e9d1ce0e1e14d11cffbfe05823a8
hroscoe287-design <hroscoe287@gmail.com>
2026-09-21 21:39:57 -0500

    Fix WebSocket adapter imports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, io, time, base64, threading, math
+import os, io, time, base64, threading, math, json, re
 from datetime import datetime, timezone
 from collections import deque
 import numpy as np
